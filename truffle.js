a30b6365c3b33eb7c607c2228b780843880edea4
Stan Takt <st@summatech.ru>
2017-11-05 21:14:01 +0300

    some cosmetic in crowdsale added Kovan network, let's deploy

--- truffle.js
+++ truffle.js
@@ -4,6 +4,14 @@ module.exports = {
       host: "localhost",
       port: 18545,
       network_id: "*" // Match any network id
-    }
+    },
+      kovan: {
+          host: "localhost",
+          port: 8545,
+          network_id: "42",
+          from: "0x00E417f2aD0018A84d1cFe4432657418e10C0d94"
+
+      }
+
   }
 };
